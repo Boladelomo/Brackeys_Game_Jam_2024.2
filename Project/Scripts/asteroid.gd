c80479f56c76d5c496798d79b07a0cb333ed9991
Boladelomo <lowcunnington@gmail.com>
2024-09-12 09:44:21 -0300

    refactor old asteroid logic prior to merge

--- Project/Scripts/asteroid.gd
+++ Project/Scripts/asteroid.gd
@@ -20,12 +20,16 @@ var current_color := empty_color
 var collided:= false
 var collision_object : CharacterBody2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_set_radius(safe_radius)
 	collider.shape.radius = _get_radius()
 	collider.position = position
 	queue_redraw()
+	print("asteroid")
+	print(str(_get_radius()))
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -35,9 +39,11 @@ func _process(delta: float) -> void:
 			current_color = safe_color
 		else:
 			current_color = empty_color
-	
+
+
 func _draw() -> void:
 	draw_circle(position, radius, current_color)
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
@@ -47,8 +53,20 @@ func _on_body_entered(body: Node2D) -> void:
 			body.player_entered_safe_area()
 	queue_redraw()
 
-func check_player_overlap(player: CharacterBody2D) -> bool:
-	return false
+
+func check_player_overlap(player: CharacterBody2D) -> bool:	
+	var rect = player.find_child("CollisionShape2D").get_shape().get_rect()
+	#TODO: change to "all corners inside circ collider"
+	var top_right = Vector2(rect.size.x, rect.position.y)
+	var bottom_left = Vector2(rect.position.x, rect.size.y)
+	var center = collider.position
+	
+	#checks that every corner of the player collider is within radius 
+	return (center.distance_to(rect.position) < radius
+	and center.distance_to(rect.position) < radius
+	and center.distance_to(top_right) < radius
+	and center.distance_to(bottom_left) < radius
+	)
 	
 func _on_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
